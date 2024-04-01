271ea2a269d2c57cef50b36f08d58298ee03c80b
fredstot <freddiestothard@hotmail.co.uk>
2024-04-01 17:26:32 +0100

    draft

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
-document.getElementById("login_button").addEventListener("click", function(){
-  document.querySelector(".popup").style.display = "flex";
-})
-document.querySelector(".close").addEventListener("click", function () {
-document.querySelector(".popup").style.display = "none" ;
-})
-var attempt = 3;
-function validation() {
-  var password = document.getElementById("password").value;
-  if(password === "GA") {
-    alert("Success!");
-    window.location="swap.html";
-    return false;
-  }
-  else{
-    if (attempt ===0) {
-      document.getElementById("password").disabled=true;
-      document.getElementById("submit").disabled=true
-      return false;
-    }
+let loginButton = document.getElementById('login')
+loginButton.addEventListener("click",handleLogin)
+function handleLogin() {
+  let myPass = document.getElementById('password')
+  if (myPass.value == 'ga') {
+  let myDiv = document.getElementById('login-container')
+  myDiv.classList.add("pop-up-dissapear")
   }
 }
-  
